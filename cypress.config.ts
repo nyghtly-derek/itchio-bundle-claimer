fbecd49762669a8e456065420344ff742b472c34
sebbu <514794+sebbu2@users.noreply.github.com>
2025-04-20 16:34:19 +0200

    fixes missing totp-generator dependencies & update cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,9 +6,12 @@ export default defineConfig({
   },
   pageLoadTimeout: 180000,
   redirectionLimit: 9001,
-  retries: 30,
+  retries: 0,
   video: false,
   watchForFileChanges: false,
+  numTestsKeptInMemory: 5,
+  experimentalMemoryManagement: true,
+  injectDocumentDomain: true,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
